9938c4d9bf823cac077a95369b6c993ee7537a68
Joey Crowe <jczwb@mail.missouri.edu>
2018-10-01 22:44:26 -0500

    Finished minimax search tenatively and waiting for testing

--- src/cs4750hw4/Node.java
+++ src/cs4750hw4/Node.java
@@ -12,15 +12,28 @@ package cs4750hw4;
 public class Node {
     private char[][] board;
     private int depth;
+    private Node miniMaxChild;
     private double heuristic;
+    
+    
     //idea: implement heuristic as a double for comparison purposes. First, evaluate heuristic(int function) and set heuristic to float. 
     //Then divide board into strata based of of distance to center. IE center = starts at .7, middle ring = .4 outer ring = .0. 
     //Then add from (0-.3) of rand(). That way the minimax search can just find the min or max based off of one number and ties 
     //are already broken automatically
+    
     public Node(){
-        board = new char[6][6];
+        board = new char[6][6];//hard coded 6x6, could possible implement for all board sizes, but for simplicity sake
         
-       
+    }
+    
+    public Node(char board[][], int x, int y, char marker){
+        this.board = new char[6][6];
+        for (int i =0; i < 6; i++){//copy board
+            for (int j=0; j < 6; j++){
+                this.board[i][j] = board[i][j];
+            }
+        }
+        this.board[x][y] = marker;
     }
     
     
@@ -28,6 +41,7 @@ public class Node {
     //uses a DFS which uses constant space. 
     // credit to https://en.wikipedia.org/wiki/Minimax for inspiration for the java algorithm
     public Node getNextNode(int depth, boolean isMaxPlayer){
+        Node tempNode;
         if (this.isTerminal()){
             return this;
         }
@@ -36,11 +50,41 @@ public class Node {
             return this;
         }
         if (isMaxPlayer){
+            this.heuristic = Double.NEGATIVE_INFINITY;
+            for (int i =0; i < 6; i++ ){//hard coded 6
+                for (int j = 0; j < 6 ; j++){
+                    if (isValid(board[i][j])){
+                        tempNode = new Node(this.board, i,j,'x');//change char to what max plays
+                        tempNode = this.getNextNode( depth - 1, false);
+                        if (tempNode.heuristic > this.heuristic){
+                            this.heuristic = tempNode.heuristic;
+                            this.miniMaxChild = tempNode;
+                        }
+                    }
+                }
+            }
             return this;
         } else {
+            this.heuristic = Double.POSITIVE_INFINITY;
+            for (int i =0; i < 6; i++ ){//hard coded 6
+                for (int j = 0; j < 6 ; j++){
+                    if (isValid(board[i][j])){
+                        tempNode = new Node(this.board, i,j,'o');//change char to whatever min plays
+                        tempNode = this.getNextNode( depth - 1, true);
+                        if (tempNode.heuristic < this.heuristic){
+                            this.heuristic = tempNode.heuristic;
+                            this.miniMaxChild = tempNode;
+                        }
+                    }
+                }
+            }
             return this;
         }
         
+    }
+    
+    public boolean isValid(char x){
+        return (x == '-');//char for blank spaces
     }
     
     //evaluate whether this is a "solution", should also set the heuristic of this node to a really large negative or positve number
